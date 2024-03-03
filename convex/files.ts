dbca6d463f7b882fd703de0ded1a855307d33b9f
swis saklo <swissaklo@gmail.com>
2024-03-03 14:03:53 +0530

    file upload completed

--- convex/files.ts
+++ convex/files.ts
@@ -1,66 +1,75 @@
-import{v,ConvexError} from "convex/values"
-import {mutation,query,MutationCtx,QueryCtx} from "./_generated/server"
-import { getUser } from "./users"
 
 
+import { ConvexError, v } from "convex/values";
+import { MutationCtx, QueryCtx, mutation, query } from "./_generated/server";
+import { getUser } from "./users";
 
-async function hasAccessToOrg(tokenIdentifier:string,orgId:string,ctx:QueryCtx | MutationCtx){
-    const user=await getUser(ctx,tokenIdentifier)
-    const hasAccess = user.orgIds.includes(orgId) ||
-    user.tokenIdentifier.includes(orgId)
-    return hasAccess
-    
+export const generateUploadUrl = mutation(async (ctx) => {
+  const identity = await ctx.auth.getUserIdentity();
+
+  if (!identity) {
+    throw new ConvexError("you must be logged in to upload a file");
+  }
+
+  return await ctx.storage.generateUploadUrl();
+});
+
+async function hasAccessToOrg(
+  ctx: QueryCtx | MutationCtx,
+  tokenIdentifier: string,
+  orgId: string
+) {
+  const user = await getUser(ctx, tokenIdentifier);
+  const hasAccess =
+    user.orgIds.includes(orgId) || user.tokenIdentifier.includes(orgId);
+  return hasAccess;
 }
 export const createFile = mutation({
-    args:{
-        name:v.string(),
-        orgId:v.string()
-    },
-    async handler(ctx,args){
-        const identity = await ctx.auth.getUserIdentity()
-        if(!identity){
-            throw new ConvexError("Unauthenticated")
-        }
-        console.log(identity)
-        const hasAccess = await hasAccessToOrg(identity.tokenIdentifier,args.orgId,ctx)
-      if(!hasAccess){
-
-          throw new ConvexError("YOu dont have access to this organization")
-      }
-
-     
-    // if( !user.orgIds.includes(args.orgId)&&user.tokenIdentifier !==identity.tokenIdentifier){
-    // }
-await ctx.db.insert("files",{
-    name:args.name,
-    orgId:args.orgId
-})
-}
-    })
-
-
-    export const getFiles = query({
-        args:{
-            orgId:v.string()
-        },
-        async handler(ctx,args){
-           
-            const identity = await ctx.auth.getUserIdentity()
-        
-            if(!identity){
-                return []
-            }
-            const hasAccess = await hasAccessToOrg(identity.tokenIdentifier,args.orgId,ctx)
-            if(!hasAccess){
-                return []
-            }
-
-            
-            return ctx.db
+  args: {
+    name: v.string(),
+    fileId: v.id("_storage"),
+    orgId: v.string(),
+  },
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new ConvexError("you must be logged in to upload a file");
+    }
+    const hasAccess = await hasAccessToOrg(
+      ctx,
+      identity.tokenIdentifier,
+      args.orgId
+    );
+    if (!hasAccess) {
+      throw new ConvexError("you do not have access to this org");
+    }
+    await ctx.db.insert("files", {
+      name: args.name,
+      orgId: args.orgId,
+      fileId: args.fileId,
+    });
+  },
+});
+export const getFiles = query({
+  args: {
+    orgId: v.string(),
+  },
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      return [];
+    }
+    const hasAccess = await hasAccessToOrg(
+      ctx,
+      identity.tokenIdentifier,
+      args.orgId
+    );
+    if (!hasAccess) {
+      return [];
+    }
+    return ctx.db
       .query("files")
       .withIndex("by_orgId", (q) => q.eq("orgId", args.orgId))
       .collect();
-        }
-    })
-
-
+  },
+});
