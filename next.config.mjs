627d32f9d72367d8877f6238c99d62d9e32ff285
swis saklo <swissaklo@gmail.com>
2024-03-04 22:36:34 +0530

    minor fixes

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        // hostname: "proficient-pheasant-194.convex.cloud",
-        hostname:"https://quaint-oyster-954.convex.cloud",
+        hostname:"quaint-oyster-954.convex.cloud",
       },
     ],
   },
